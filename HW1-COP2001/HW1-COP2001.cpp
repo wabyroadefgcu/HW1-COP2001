9b2af7941f3d5f18752ad67f98b10b8104f39168
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2018-02-06 09:47:45 -0500

    MVP1 - revision that was turned in for grading.

--- HW1-COP2001/HW1-COP2001.cpp
+++ HW1-COP2001/HW1-COP2001.cpp
@@ -3,15 +3,49 @@
 
 #include "stdafx.h"
 
+void PrintSchedule(double intRate, double initialPrincipal, double paymentAmount) {
+	double currentPrincipal = initialPrincipal;
+	double monthlyInterest = intRate / 12;
+	double interestThisMonth;
+	double paymentOnPrincipal;
+	int paymentCounter = 0;
+	//	TODO: Possible Improvement - Add dollar sign to table data for currency amounts
+	//	char dollarSign[] = "$";
+	//  printf("%10s%.2f", dollarSign, initialPrincipal);   -  unsuccessful attempt at including dollar sign
 
-int main()
-{
-	printf("Hello World.\n");
-//	std::getchar();
-	system("pause");
-// system(pause) is platform specific, get rid of it, try this below:
-//	std::cout << "Press enter to continue ...";
-//	std::cin.get();
-    return 0;
+	printf("The initial loan is $%.2f, \n", initialPrincipal);
+	printf("The interest rate is %.2f, \n", intRate);
+	printf("The payment amount is $%.2f \n", paymentAmount);
+
+	printf("\n");
+	printf("               Interest     Principal    Ending       Payment\n");
+	printf(" Principal       Paid         Paid       Payment      Balance     made on: \n");
+	printf("__________   __________   __________   __________   __________   ____________\n");
+	while (currentPrincipal > 0) {
+		paymentCounter++;
+		interestThisMonth = currentPrincipal * monthlyInterest;
+		paymentOnPrincipal = paymentAmount - interestThisMonth;
+		if (paymentOnPrincipal > currentPrincipal) {
+			paymentOnPrincipal = currentPrincipal;
+			paymentAmount = currentPrincipal + interestThisMonth;
+		}
+		printf("%*.2f   ", 10, currentPrincipal);
+		printf("%*.2f   ", 10, interestThisMonth);
+		printf("%*.2f   ", 10, paymentOnPrincipal);
+		printf("%*.2f   ", 10, paymentAmount);
+		currentPrincipal = currentPrincipal - paymentOnPrincipal;
+		printf("%*.2f   ", 10, currentPrincipal);
+		printf("____________\n");
+	}
 }
-
+int main() {
+	double initialPrincipal, interestRate, paymentAmount;
+	printf("Enter the amount of the loan : ");
+	scanf("%lf", &initialPrincipal);
+	printf("\nEnter the interest rate(as decimcal eg: .04) : ");
+	scanf("%lf", &interestRate);
+	printf("\nEnter the payment amount per month : ");
+	scanf("%lf", &paymentAmount);
+	PrintSchedule(interestRate, initialPrincipal, paymentAmount);
+	return 0;
+}
